3b67adc6aa9bce92c650ae4e01809b051098bfd1
sara-prioriello <saraprioriello6@gmail.com>
2026-03-15 11:43:13 +0100

    add click and eventListner

---
+++ main.js
@@ -0,0 +1,9 @@
+//seleziono un elemento del dom quindo il bottone
+const buttonEl = document.querySelector('button');
+
+/*//quando faccio cliock sul bottone deve apparire un messaccio per esser sicura di aver clickato
+buttonEl.addEventListener('click', function() {
+   console.log('tasto cliccato!') 
+ });*/
+
+ 
